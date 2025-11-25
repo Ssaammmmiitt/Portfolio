47f52cbc9ad0a3dc612fd1b38ea5c4692bbe7161
Sammit Poudyal <poudyal.sammit@gmail.com>
2025-11-25 17:54:00 +0545

    feat: add new font sources and update styling in Work section

--- src/Sections/Work.jsx
+++ src/Sections/Work.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import GradientButton from '../Components/GradientButton'
+
+
 
 const Work = () => {
   return (
-    <div>
-      Work
-    </div>
+    <>
+      <div className='h-screen bg-white text-black py-24 lg:py-40'>
+        {/*title*/}
+        <div className='main-container pb-8 lg:pb-12 max-md:flex-col flex gap-6 justify-between items-start md:items-end'>
+          <div className='max-w-xl'>
+            <h3 className='mb-3'>Selected Works</h3>
+            <div className="text-lg lg:text-xl">A showcase of my projects and collaborations.</div>
+          </div>
+          <GradientButton text="View Works" className='btn-light ' link="/" />
+        </div>
+
+
+        {/*projects*/}
+        <div className="flex gap-4">
+          <a href=''>
+            <img src="" alt =" " />
+            <span></span>
+          </a>
+        </div>
+      </div>
+    </>
   )
 }
 
